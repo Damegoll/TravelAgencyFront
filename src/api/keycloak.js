7992b4a872f894203642ca8e21cabc7aec2f8f78
Damegoll <ignacioandrestapiadonaire@gmail.com>
2026-05-04 17:14:30 -0400

    minor change: you know the drill....

--- src/api/keycloak.js
+++ src/api/keycloak.js
@@ -1,5 +1,11 @@
 import Keycloak from "keycloak-js";
 
+keycloak.init({
+    onLoad: 'check-sso',
+    checkLoginIframe: false,
+    pkceMethod: 'S256',
+})
+
 
 const keycloak = new Keycloak({
     url: "https://146.190.78.157:8443",
