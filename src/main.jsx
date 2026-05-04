6203e8fa57df2ecc003a625305adfac93e86c044
Damegoll <ignacioandrestapiadonaire@gmail.com>
2026-05-04 16:54:23 -0400

    change: keycloak.js is now where it should be and the main.jsx does point correctly

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import keycloak from './keycloak'
+import keycloak from './api/keycloak'
 
 keycloak.init({
   onLoad: 'login-required',
